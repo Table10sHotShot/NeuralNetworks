c4948ae2d123a0d4b23f1947c355cdd447931fe7
alex <Table10sHotShot@gmail.com>
2014-12-12 18:17:36 -0500

    Created PyCharm project
    Updated RNN style
    PyCharm helper files added

--- RNN.py
+++ RNN.py
@@ -6,11 +6,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def logistic_fn(z):
     return 1/(1+np.exp(-z))
 
+
 def dlogistic_fn(z):
     return logistic_fn(z)*(1-logistic_fn(z))
+
 
 class RNN(object):
 
@@ -18,45 +21,47 @@ class RNN(object):
 
         self._num_hidden_neurons = num_hidden_neurons
         
-#        initialize weights
+        # initialize feed forward weights
         self._ff_weights = []
         self._ff_weights.append(np.random.rand(3, num_hidden_neurons))
         self._ff_weights.append(np.random.rand(num_hidden_neurons+1, 1))
 
-        self._time_weights = [np.random.rand(num_hidden_neurons, num_hidden_neurons), np.random.rand(1,1)]
+        # initialize recurrent weights
+        self._time_weights = [np.random.rand(num_hidden_neurons, num_hidden_neurons), np.random.rand(1, 1)]
 
+        # initialize unit values for recurrent connections
         self._x_hidden_init = np.append([1], np.random.rand(num_hidden_neurons))
-        self._x_output_init = np.array([.5])
+        # self._x_output_init = np.array([.5])
 
-#    reset training example dependent variables
-    def reset(self):
+    # reset training example dependent variables
+    def _reset(self):
 
-        self._xs_input = []
-        self._xs_hidden = [self._x_hidden_init]
-        self._xs_output = []#self._x_output_init]
         self._zs_hidden = []
         self._zs_output = []
+        self._xs_input = []
+        self._xs_hidden = [self._x_hidden_init]
+        self._xs_output = []
 
-        self._ff_weight_gradient = [np.zeros((3,self._num_hidden_neurons)), np.zeros((self._num_hidden_neurons+1,1))]
-        self._time_weight_gradient = [np.zeros((self._num_hidden_neurons, self._num_hidden_neurons)), np.zeros((1,1))]
+        self._ff_weight_gradient = [np.zeros((3, self._num_hidden_neurons)), np.zeros((self._num_hidden_neurons+1, 1))]
+        self._time_weight_gradient = [np.zeros((self._num_hidden_neurons, self._num_hidden_neurons)), np.zeros((1, 1))]
 
     def feed_forward(self, input_sequence1, input_sequence2):
 
         for int1, int2 in zip(input_sequence1, input_sequence2):
-#            create input array
-            x_input = np.array([1,int1, int2])
+            # create input array
+            x_input = np.array([1, int1, int2])
             self._xs_input.append(x_input)
             
-#            calculate signal going into hidden layer - includes input from previous timestep hidden layer
+            # calculate signal going into hidden layer - includes input from previous timestep hidden layer
             z_hidden = np.dot(x_input, self._ff_weights[0]) + np.dot(self._xs_hidden[-1][1:], self._time_weights[0])
             self._zs_hidden.append(z_hidden)
-            
-#            calculate hidden layer output
+
+            # calculate hidden layer output
             x_hidden = np.append([1], logistic_fn(z_hidden))
             self._xs_hidden.append(x_hidden)
 
-#            calculate signal going into ouput layer - includes input from previous timestep output layer
-            z_output = np.dot(x_hidden, self._ff_weights[1])# + np.dot(self._xs_output[-1], self._time_weights[1])
+            # calculate signal going into output layer
+            z_output = np.dot(x_hidden, self._ff_weights[1])
             self._zs_output.append(z_output)
 
 #            calculate output layer output
@@ -65,54 +70,50 @@ class RNN(object):
 
     def back_propagate(self, targets, learning_rate):
 
-        enumerated_targets = list(enumerate(targets))
-        
-        for timestep, target in reversed(enumerated_targets):
+        enum_targets = list(enumerate(targets))
 
-#            calculate dedz for output layer
-            output = self._xs_output[timestep]
-            z_output = self._zs_output[timestep]
+        for idx, target in reversed(enum_targets):
+
+            # calculate dedz for output layer
+            output = self._xs_output[idx]
+            z_output = self._zs_output[idx]
             dedz_output = (output - target) * dlogistic_fn(z_output)
 
-#            calculate dedz for output layer assuming z is actual output
-#            output = self._zs_output[timestep]
-#            dedz_output = (output - target)
-
-#            calculate dedx for hidden layer
+            # calculate dedx for hidden layer
             dedx_hidden = np.dot(self._ff_weights[1], dedz_output)
             
-#            calculate dedz for hidden layer
-            z_hidden = self._zs_hidden[timestep]
+            # calculate dedz for hidden layer
+            z_hidden = self._zs_hidden[idx]
             dedz_hidden = dedx_hidden[1:] * dlogistic_fn(z_hidden)
 
-#            calculate dedws
-            dedw_ff_output = np.outer(self._xs_hidden[timestep+1], dedz_output)
-            dedw_ff_hidden = np.outer(self._xs_input[timestep], dedz_hidden)
-#            dedw_time_output = np.outer(self._xs_output[timestep], dedz_output)
-            dedw_time_hidden = np.outer(self._xs_hidden[timestep][1:], dedz_hidden)
+            # calculate dedws
+            dedw_ff_output = np.outer(self._xs_hidden[idx+1], dedz_output)
+            dedw_ff_hidden = np.outer(self._xs_input[idx], dedz_hidden)
+            # dedw_time_output = np.outer(self._xs_output[timestep], dedz_output)
+            dedw_time_hidden = np.outer(self._xs_hidden[idx][1:], dedz_hidden)
             
-#            update weight gradients
+            # update weight gradients
             self._ff_weight_gradient[1] += dedw_ff_output
             self._ff_weight_gradient[0] += dedw_ff_hidden
-#            self._time_weight_gradient[1] += dedw_time_output
+            # self._time_weight_gradient[1] += dedw_time_output
             self._time_weight_gradient[0] += dedw_time_hidden
-        
-#        update initial output activity vector
-        dedx_output_init = np.dot(self._time_weights[1], dedz_output)
-        self._x_output_init -= learning_rate * dedx_output_init
 
-#        update initial hidden activity vector
+        # update initial output activity vector
+        # dedx_output_init = np.dot(self._time_weights[1], dedz_output)
+        # self._x_output_init -= learning_rate * dedx_output_init
+
+        # update initial hidden activity vector
         dedx_hidden_init = np.dot(self._time_weights[0], dedz_hidden)
         dedx_hidden_init = np.append([0], dedx_hidden_init)
         self._x_hidden_init -= learning_rate * dedx_hidden_init
 
-        eff_learning_rate = learning_rate / len(enumerated_targets)
+        eff_learning_rate = learning_rate / len(enum_targets)
 
-#        update weights
+        # update weights
         self._ff_weights[0] -= eff_learning_rate * self._ff_weight_gradient[0]
         self._ff_weights[1] -= eff_learning_rate * self._ff_weight_gradient[1]
         self._time_weights[0] -= eff_learning_rate * self._time_weight_gradient[0]
-#        self._time_weights[1] -= eff_learning_rate * self._time_weight_gradient[1]
+        # self._time_weights[1] -= eff_learning_rate * self._time_weight_gradient[1]
 
     def train_sequence(self, input_sequence1, input_sequence2, target_sequence, learning_rate):
 
@@ -122,7 +123,7 @@ class RNN(object):
 
 if __name__ == "__main__":
 
-    logging.basicConfig(format='{levelname}: {message}', style='{', level=logging.INFO)
+    logging.basicConfig(format='{levelname}: {message}', style='{', level=logging.DEBUG)
 
     pkl_filename = 'rnn_4hu.pkl'
 
@@ -131,13 +132,14 @@ if __name__ == "__main__":
         logger.info("loaded rnn")
         logger.debug('ff weights', str(rnn._ff_weights))
         logger.debug('time weights', str(rnn._time_weights))
-    else: rnn = RNN(4)
+    else:
+        rnn = RNN(4)
 
-    num_training_exs = 2e4
+    num_training_exs = 2e5
     learning_rate = .04
     
     i=0
-    for num1, num2 in np.random.randint(0,63, (num_training_exs,2)):
+    for num1, num2 in np.random.randint(0, 63, (num_training_exs, 2)):
         
         i+=1
         perc = i / num_training_exs
@@ -159,12 +161,12 @@ if __name__ == "__main__":
         input_sequence2 = reversed(list(map(int, str2)))
         target_sequence = reversed(list(map(int, str_target)))
 
-        rnn.reset()
+        rnn._reset()
         rnn.train_sequence(input_sequence1, input_sequence2, target_sequence, learning_rate)
 
         if num_training_exs - i < 10:
             logger.debug(str1 + ' ' + str2 + ' ' + str_target)
-            predicted = ''.join(list( map(lambda x: str(int(round(x))), np.ravel(rnn._xs_output)[::-1]) ))
+            predicted = ''.join(list(map(lambda x: str(int(round(x))), np.ravel(rnn._xs_output)[::-1]) ))
             logger.debug(predicted)
             logger.info(predicted == str_target)
 
